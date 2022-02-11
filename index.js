06ec788b9dbc8fa46265bfc0b40704886d221007
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-02-11 21:11:01 +0200

    added task2

--- index.js
+++ index.js
@@ -9,11 +9,33 @@
 // 1.пременная
 // 2. ввод
 
-const userInput = prompt("Какое официальное название JavaScript?");
+// const userInput = prompt("Какое официальное название JavaScript?");
+// const NAMEJS = 'ecmascript';
 
-if ( userInput === "ECMAScript") {
-    alert("Верно!");
+// // if ( userInput.toLowerCase() === NAMEJS) {
+// //     alert("Верно!");
     
-} else {
-    alert("Не знаете? ECMAScript!")
-}
+// // } else {
+// //     alert("Не знаете? ECMAScript!")
+// // }
+// const comparison = userInput.toLowerCase() === NAMEJS;
+// const message = comparison ? "Верно!" : "Не знаете? ECMAScript!";
+// alert(message);
+
+//2. Напишите программу, которая получит от пользователя
+//число (количество минут) и выведет в консоль
+//строку в формате часов и минут
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+// 70 === 01:10
+
+const userInput = prompt('Введите количество минут');
+const hours = Math.floor(userInput / 60);
+const modifyHours = String(hours).padStart(2, 0);
+
+const minutes = userInput % 60;
+
+const modifyMinutes = String(minutes).padStart(2, 0);
+
+console.log(`${modifyHours}:${modifyMinutes}`);
+
+

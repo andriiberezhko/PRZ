3ced6e251ed873e8a5cac8837bd09a53b1b7cd01
zganz <humanmailer@gmail.com>
2022-04-08 21:34:29 +0300

    unique array

--- 2.js
+++ 2.js
@@ -21,17 +21,51 @@
 //нужно добавить проверку, что функция получает числа
 
 
-const minNumber = function (a, b) {
-    // if (a > b) {
-    //     return b;
-    // }
-    // else {
-    //     return a;
-    // }
-    if (typeof a !== 'number' && typeof b !== 'number') {
-        return 'one of the arguments is string';
-    }
-    return a > b ? b : a;
+// const minNumber = function (a, b) {
+//     // if (a > b) {
+//     //     return b;
+//     // }
+//     // else {
+//     //     return a;
+//     // }
+//     if (typeof a !== 'number' && typeof b !== 'number') {
+//         return 'one of the arguments is string';
+//     }
+//     return a > b ? b : a;
     
-}
-console.log (minNumber('this is another string', 11))
+// }
+// console.log (minNumber('this is another string', 11))
+
+
+
+
+//Напишите функцию logItems(array) которая принимает
+//массив и использует цикл for, который для каждого
+//элемента массива будет выводить сообщение в формате
+//<номер элемента> - <значение элемента>
+//Нумерация элементов должна начинаться с 1.
+
+
+// function logItems(array){
+// for(let i = 0; i<array.length; i+=1){
+//     console.log(`Номер элемента ${i + 1} значение элемента ${array[i]}`);
+// }
+// }
+
+// logItems(['Джаз', 'Блюз', 'Рок-н-ролл', 'Регги', 'Рэп'])
+
+
+// const styles = ['Джаз', 'Джаз','Блюз', 'Рок-н-ролл', 'Регги', 'Рэп'];
+// const filteredStyles = styles.filter((element, index, array) => array.indexOf(element) === index);
+// console.log(filteredStyles);
+// const d =`test`;
+// const f ={};
+
+// const a = {
+//     b:1,
+//     c:2
+
+// }
+// a[`d`] = d;
+// a[`f`] = 4;
+// console.log(a);
